71f6ed4767368bc162993b1d6554f95bcf4cb977
Cem Arda Fil <cemarda44@gmail.com>
2024-08-22 01:10:46 +0300

    App.js accepts HTML.

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Vision-Bridge
 Software Developer Assignment
 
-Requires js-yaml module for parsing. (npm install js-yaml)
+Requires js-yaml module for parsing YAML files. (npm install js-yaml)
+Requires jsdom module for DOM manipulation for Node.js. (npm install jsdom)
 Put your YAML(".yml") files in the same directory with "app.js". Javascript file will automatically detect and apply them.
 YAML file must include the indicators ".", "#" before element names so that document.querySelectorAll() can understand if it is searching for Id, Class or Tag.
 Insert action only accepts positions "after" and "before" so raises a console error if there is an undefined position.
-You need to put "<script defer src="./app.js"></script>" line at the end of body of the HTML you wish to apply the configs to.
--- app.js
+++ app.js
@@ -1,89 +1,103 @@
 const fs = require('fs'); // File system module to read YAML files
 const yaml = require('js-yaml'); // YAML file parser
+const { JSDOM } = require('jsdom');
 
+// Load HTML document
+const htmlDocument = fs.readFileSync('index.html', 'utf-8');
+const dom = new JSDOM(htmlDocument);
+
+// Now you can manipulate the document as you would with any other DOM Document
+let htmlDOM = dom.window.document;
+
+// Search through directory to find YAML files
 const files = fs.readdirSync("./");
-let configFiles = []; 
-for (file of files){
+let configFiles = []; // All YAML files in directory
+for (const file of files) {
     if (file.includes(".yml")) {
-        configFiles.push(file)
+        configFiles.push(file);
     }
 }
 
-let configs = [];
+// If there is no YAML file or no YAML file found, give a warning
+if (configFiles.length === 0) {
+    console.warn("No configuration YAML file found!");
+}
 
-// TODO: Error Catch For Bad YAML Files
+// Pushing YAML file configurations to an array
+let configs = []; // Unsorted configurations
 
 for (const configFile of configFiles) {
-    let tempConfigs = yaml.load(fs.readFileSync(configFile, "utf-8")); // Read YAML file and parse it as config array
-    configs.push(...tempConfigs.actions);
-    tempConfigs = null;
+    try {
+        let tempConfigs = yaml.load(fs.readFileSync(configFile, "utf-8")); // Read YAML file and parse it as an object with actions array
+        configs.push(...tempConfigs.actions); // Push new configurations to an unsorted array
+    } catch (error) {
+        console.error(`Bad YAML file. Broken file: ${configFile}`);
+    }
 }
 
 // Priority Order
 const priority = ["remove", "replace", "insert", "alter"];
-const sortedConfigs = [];
+const sortedConfigs = []; // Sorted actions based on priority
 
+// Sort actions based on priority
 for (const action of priority) {
     configs.forEach((config, idx) => {
-        if (action === config["type"] && configs.lenght !== 0) {
-            const halfBefore = configs.slice(0, idx)
-            const halfAfter = configs.slice((idx))
-            configs.actions = halfBefore.concat(halfAfter)
-            sortedConfigs.push(config);
+        if (action === config["type"] && configs.length !== 0) {
+            sortedConfigs.push(config); // Push action to sorted array
+        }
+    });
+}
+// Creating DOM manipulation action functions
+function removeAction(query, htmlFile) {
+    const elementsToRemove = htmlFile.querySelectorAll(query); // Detect elements based on query (element name)
+    if (elementsToRemove.length !== 0) {
+        elementsToRemove.forEach(element => element.remove()); // Remove them from DOM
+    }
+}
+
+function replaceAction(query, newElement, htmlFile) {
+    const elementsToReplace = htmlFile.querySelectorAll(query);
+    if (elementsToReplace.length !== 0) {
+        elementsToReplace.forEach(element => element.outerHTML = newElement); // Replace the whole element with the new element
+    }
+}
+
+function insertAction(position, target, elementToInsert, htmlFile) {
+    const targetLocation = htmlFile.querySelector(target); // Find the target location
+    if (targetLocation) {
+        if (position === "after") {
+            targetLocation.insertAdjacentHTML("afterend", elementToInsert); // Insert element after the target
+        } else if (position === "before") {
+            targetLocation.insertAdjacentHTML("beforebegin", elementToInsert); // Insert element before the target
+        } else {
+            console.error(`Undefined insert position. Input: ${position}`); // Raise console error if position parameter not applicable
         }
     }
-    );
-}
-// console.log(sortedConfigs)
-
-// Creating DOM manipulation action functions
-// TODO: Error catch
-
-function removeAction(query) {
-    const elementsToRemove = document.querySelectorAll(query); // Detect elements based on query (element name)
-    elementsToRemove.forEach(element => element.remove()); // Remove them from DOM
 }
 
-function replaceAction(query, newElement) {
-    const elementsToReplace = document.querySelectorAll(query);
-    elementsToReplace.forEach(element => element.outerHTML = newElement); // Replace the whole element with the new element
-}
-
-function insertAction(position, target, elementToInsert) {
-    const targetLocation = document.querySelector(target); // Find the target location
-    if (position === "after") {
-        targetLocation.insertAdjacentHTML("afterend", elementToInsert); // Insert element after the target
-    }
-    else if (position === "before") {
-        targetLocation.insertAdjacentHTML("beforebegin", elementToInsert); // Insert element before the target
-    }
-    else {
-        console.error(`Undefined position. Input: ${position}`); // Raise console error if position parameter not applicable
-    }
-}
-
-function alterAction(query, newValue) {
-    const bodyInnerText = Document.body.innerText; // Copy original text
-    document.body.innerText = bodyInnerText.replace(query, newValue) // mutate copy then assign it to text
+function alterAction(query, newValue, htmlFile) {
+    const bodyInnerText = htmlFile.body.innerHTML; // Copy original text
+    htmlFile.body.innerHTML = bodyInnerText.replaceAll(query, newValue); // Mutate copy then assign it to text
 }
 
 // Applying Configurations
-
 for (const configuration of sortedConfigs) {
     switch (configuration["type"]) {
         case "remove":
-            removeAction(configuration["selector"]);
+            removeAction(configuration["selector"], htmlDOM);
             break;
         case "replace":
-            replaceAction(configuration["selector"], configuration["newElement"]);
+            replaceAction(configuration["selector"], configuration["newElement"], htmlDOM);
             break;
         case "insert":
-            insertAction(configuration["position"], configuration["target"], configuration["element"]);
+            insertAction(configuration["position"], configuration["target"], configuration["element"], htmlDOM);
             break;
         case "alter":
-            alterAction(configuration["oldValue"], configuration["newValue"]);
+            alterAction(configuration["oldValue"], configuration["newValue"], htmlDOM);
             break;
         default:
-            console.error(`Undefined action. Skipped. Action: ${configuration}`)
+            console.log(`Undefined action. Skipped. Action: ${configuration}`); // Unreachable sortedConfigs only accepts available configs. 
     }
 }
+
+console.log(htmlDOM.documentElement.outerHTML)
